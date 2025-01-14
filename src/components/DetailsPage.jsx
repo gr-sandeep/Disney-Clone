985fd7145c56c33fda082452643d0186266365cf
sandeep-gr <sandeepg8@hexaware.com>
2025-01-14 09:24:06 +0530

    details page fix

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,18 +1,52 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { MoviesList } from "../config/MoviesList";
+import { IoAdd, IoAddCircleOutline, IoPlayOutline } from "react-icons/io5";
+import { MdAdd, MdOutlineMovieFilter } from "react-icons/md";
+import { HiOutlineUserGroup } from "react-icons/hi";
 
 const DetailsPage = () => {
   const { id } = useParams();
   const movie = MoviesList.find((movie) => movie.id === parseInt(id));
 
   return (
-    <div className="mt-20">
-      <img src={movie.backgroundImg} className="w-full h-full" alt="" />
+    <div>
+      <img
+        className="relative mt-24 rounded-md w-full h-[calc(100vh-96px)]"
+        src={movie.backgroundImg}
+        alt={movie.title}
+      />
+      <div className="absolute bottom-20 px-10 w-1/2">
+        <img src={movie.titleImg} alt={movie.title} className="w-[400px] h-[400px] mb-5" />
+        <div className="flex items-center justify-start gap-5 mb-5">
+          <button
+            title="Play Movie"
+            className="border cursor-pointer flex items-center gap-2 p-2 rounded font-semibold px-4 tracking-wider hover:bg-[#f9f9f9] hover:text-[#090b13]"
+          >
+            <IoPlayOutline fontSize={32} /> PLAY
+          </button>
+          <button
+            title="Watch Trailer"
+            className="border cursor-pointer flex items-center gap-2 p-2 rounded font-semibold px-4 tracking-wider hover:bg-[#f9f9f9] hover:text-[#090b13]"
+          >
+            <MdOutlineMovieFilter fontSize={32} /> TRAILER
+          </button>
 
-      <h1>{movie.title}</h1>
-      <h1>{movie.description}</h1>
-      <h1>{movie.subTitle}</h1>
+          <MdAdd
+            fontSize={40}
+            className="border-2 cursor-pointer rounded-full p-1 hover:bg-[#f9f9f9] hover:text-[#090b13]"
+            title="Add to WatchList"
+          />
+
+          <HiOutlineUserGroup
+            fontSize={40}
+            className="border-2 cursor-pointer rounded-full p-1 hover:bg-[#f9f9f9] hover:text-[#090b13]"
+            title="Group Notify"
+          />
+        </div>
+        <h1 className="font-semibold pb-5">{movie.subTitle}</h1>
+        <h1>{movie.description}</h1>
+      </div>
     </div>
   );
 };
